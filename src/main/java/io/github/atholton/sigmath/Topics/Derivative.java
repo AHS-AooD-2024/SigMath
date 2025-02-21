a4afcfbec067075d0fe317ac63ded00b20ef2b91
dxmbname <154003522+dxmbname@users.noreply.github.com>
2025-02-21 08:57:48 -0500

    Polynomial Derivative Class
    added derivative and polynomial derivative class

---
+++ src/main/java/io/github/atholton/sigmath/Topics/Derivative.java
@@ -0,0 +1,14 @@
+package io.github.atholton.sigmath.Topics;
+
+import java.util.ArrayList;
+
+public class Derivative extends Topic{
+    public Derivative() {
+        proficiencyLevel = 1;
+        formulaList = new ArrayList<String>();
+    }
+
+    public Node returnAnswer(Node question, int listIndex) {
+        
+    }
+}
---
+++ src/main/java/io/github/atholton/sigmath/Topics/PolynomialDerivative.java
@@ -0,0 +1,12 @@
+package main.java.io.github.atholton.sigmath.Topics;
+
+import java.util.ArrayList;
+
+public class PolynomialDerivative extends Derivative{
+    public PolynomialDerivative() {
+        super();
+        formulaList.add("a*x^n");
+        formulaList.add("a*x^n + b*x^n-1");
+        formulaList.add("a*x^n + b*x^n-1 + c*x^n-2");
+    }
+}
