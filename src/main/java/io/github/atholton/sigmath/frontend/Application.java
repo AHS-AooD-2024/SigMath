564f62c55fcaa04c32db10a7aee401402b2bb34e
dxmbname <154003522+dxmbname@users.noreply.github.com>
2025-03-13 09:25:44 -0400

    test
    more test

--- src/main/java/io/github/atholton/sigmath/frontend/Application.java
+++ src/main/java/io/github/atholton/sigmath/frontend/Application.java
@@ -26,10 +26,11 @@ public class Application extends JFrame implements Runnable
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        MainMenu mm = MainMenu.get();
+        LoadingMenu lm = new LoadingMenu();
+        //MainMenu mm = MainMenu.get();
         //AllTopicsMenu m = AllTopicsMenu.get();
         //add(mm);
-        add(mm);
+        add(lm);
 
         pack();
     }
