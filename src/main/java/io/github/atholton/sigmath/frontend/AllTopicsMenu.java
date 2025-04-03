e4af09474fc18fa3fa4ab0860ff9b8fc3a290a30
Charush <charush.sai@gmail.com>
2025-04-03 09:14:31 -0400

    adhfdafidhyi

--- src/main/java/io/github/atholton/sigmath/frontend/AllTopicsMenu.java
+++ src/main/java/io/github/atholton/sigmath/frontend/AllTopicsMenu.java
@@ -3,6 +3,9 @@ package io.github.atholton.sigmath.frontend;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -15,8 +18,17 @@ import io.github.atholton.sigmath.topics.Topic;
 public class AllTopicsMenu extends Menu {
     private ArrayList<TopicsButton> topics;
     private static AllTopicsMenu instance;
+    private GridBagLayout layout;
     
     private AllTopicsMenu() {
+        super();
+        layout = new GridBagLayout();
+        setLayout(layout);
+        c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+
+        c.insets = new Insets(30, 30, 30, 30); 
+
         topics = new ArrayList<TopicsButton>();
 
         addTopic(PolynomialDerivative.get(), "Polynomial Derivatives");
@@ -25,7 +37,7 @@ public class AllTopicsMenu extends Menu {
             add(t);
         }
     }
-    public void addTopic(Topic t, String topicString) {
+    public void addTopic(Topic t, String topicString) { 
         TopicsButton b = new TopicsButton(t, topicString);
         b.setSize(150, 75);
         b.setMaximumSize(getSize());
